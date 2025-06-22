b7fc5ba0f58381835475878c635aeb65a7b17914
MZahidKamal <kamal.md.zahid@gmail.com>
2025-06-22 02:05:55 +0200

    MongoDB and Mongoose are installed and the connection is stablished using Mongoose.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import {Server} from 'http';
 import app from "./app";
+import 'dotenv/config';
+import * as mongoose from "mongoose";
 
 
 const PORT = 5000;
@@ -8,11 +10,15 @@ let server: Server;
 
 const bootstrap = async () => {
     try {
+
+        // Connecting to MongoDB, using the Mongoose package. MongoDB URI is coming from the.env file.
+        await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ktxyk.mongodb.net/Library_Management_API_DB-PHNLB5-M3A3?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log('Connected to MongoDB using Mongoose!');
+
         // Running the server to see the output in the browser.
         server = app.listen(PORT, () => {
-            console.log(`Library Management server listening on port ${PORT}`);
+            console.log(`Library Management Server listening on port ${PORT}`);
         });
-
     }
     catch (error) {
         console.log('Error in bootstrap!');
