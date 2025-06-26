7c6296b7788265b5c9da61bc0888ff343a1d29aa
MZahidKamal <kamal.md.zahid@gmail.com>
2025-06-26 16:55:01 +0200

    APIs to create a new borrow entry has been created successfully.

--- src/app/controllers/borrows.controller.ts
+++ src/app/controllers/borrows.controller.ts
@@ -6,7 +6,21 @@ const borrowRoutes: Router = express.Router();
 
 // CREATE
 borrowRoutes.post('/', async (req: Request, res: Response) => {
-
+    try {
+        const newBorrowObj = req.body;
+        const newBorrow = await BorrowModel.create(newBorrowObj);
+        res.status(201).json({
+            success: true,
+            message: 'New borrow entry created successfully!',
+            data: newBorrow
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            message: 'New borrow entry creation failed!',
+            success: false,
+            error: error.errors
+        })
+    }
 });
 
 // READ
